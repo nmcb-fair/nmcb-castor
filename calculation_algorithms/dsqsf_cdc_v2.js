5f2e6885618b7fb8195bea4777c6dc1c6eead224
Shuxin Zhang <s.x.zhang1201@gmail.com>
2024-04-12 14:52:26 +0200

    Update dsqsf_cdc_v2.js
    Refine comments

--- calculation_algorithms/dsqsf_cdc_v2.js
+++ calculation_algorithms/dsqsf_cdc_v2.js
@@ -1,21 +1,21 @@
 '##allowempty##';
 var fatigue = 0;
 
-'Fatigue'
+// 'Fatigue' (Q1)
 if({dsq_fatigue_freq}>=1 && {dsq_fatigue_sev}>=1){
 fatigue+=1;
 }
 else{
 fatigue+=0};
 
-'Soreness'
+// 'Soreness' (Q2)
 var sore = 0;
 if({dsq_soreness_freq}>=1 && {dsq_soreness_sev}>=1){
 sore+=1;}
 else{
 sore+=0};
 
-'Exercise intolerance'
+// 'Exercise intolerance' (Q3)
 var excs = 0;
 if({dsq_minexercise_freq}>=1 && {dsq_minexercise_sev}>=1){
 excs+=1;
@@ -23,6 +23,7 @@ excs+=1;
 else{
 excs+=0};
 
+// Post-Exertional Malaise (Q2 or Q3)
 var pem = 0;
 if(sore + excs >=1){
 pem+=1;
@@ -30,6 +31,7 @@ pem+=1;
 else{
 pem+=0};
 
+// Q4: Unrefreshing Sleep
 var sleep = 0;
 if({dsq_unrefreshed_freq}>=1 && {dsq_unrefreshed_sev}>=1){
 sleep+=1;
@@ -37,27 +39,32 @@ sleep+=1;
 else{
 sleep+=0};
 
+// Q7:  Problems remembering things
 var memory = 0;
-var atten = 0;
-var cog = 0;
 if({dsq_memory_freq}>=1 && {dsq_memory_sev}>=1){
 memory+=1;
 }
 else{
 memory+=0};
 
+// Q8: Difficulty paying attention for a long period of time
+var atten = 0;
 if({dsq_attention_freq}>=1 && {dsq_attention_sev}>=1){
 atten+=1;
 }
 else{
 atten+=0};
 
+// Memory / Concentration (Question 7 or 8)
+var cog = 0;
 if(memory + atten >= 1){
 cog+=1;
 }
 else{
 cog+=0};
 
+
+// Q5: Pain or aching in your muscles
 var pain = 0;
 if({dsq_pain_freq}>=1 && {dsq_pain_sev}>=1){
 pain+=1;
@@ -65,6 +72,7 @@ pain+=1;
 else{
 pain+=0};
 
+// Q13:  Flu-like symptoms
 var immun = 0;
 if({dsq_flu_freq}>=1 && {dsq_flu_freq}>=1){
 immun+=1;
@@ -72,6 +80,10 @@ immun+=1;
 else{
 immun+=0};
 
+/*
+At least 1 symptom (frequency and severity ratings >=1)
+ from at least 4 symptom domains:
+ */
 var symptoms = 0;
 if(pem + sleep + cog + pain + immun>=4){
 symptoms+=1;
