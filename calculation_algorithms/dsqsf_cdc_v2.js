041ceb8e8269c76dc119a7cc8bcd1987d6713665
Shuxin Zhang <s.x.zhang1201@gmail.com>
2024-04-12 13:09:50 +0200

    Update dsqsf_cdc_v2.js

---
+++ calculation_algorithms/dsqsf_cdc_v2.js
@@ -0,0 +1,77 @@
+'##allowempty##';
+var fatigue = 0;
+
+if({dsq_fatigue_freq}>=1 && {dsq_fatigue_sev}>=1){
+fatigue+=1;
+}
+else{
+fatigue+=0};
+
+var sore = 0;
+var excs = 0;
+var pem = 0;
+
+if({dsq_soreness_freq}>=1 && {dsq_soreness_sev}>=1){
+sore+=1;}
+else{
+sore+=0};
+
+if({dsq_minexercise_freq}>=1 && {dsq_minexercise_sev}>=1){
+excs+=1;
+}
+else{
+excs+=0};
+if(sore + excs >=1){
+pem+=1;
+}
+else{
+pem+=0};
+var sleep = 0;
+if((Object.values(obj[3])[0])>=1 && (Object.values(obj[3])[1])>=1){
+sleep+=1;
+}
+else{
+sleep+=0};
+var memory = 0;
+var atten = 0;
+var cog = 0;
+if({dsq_unrefreshed_freq}>=1 && {dsq_unrefreshed_sev}>=1){
+memory+=1;
+}
+else{
+memory+=0};
+if({dsq_memory_freq}>=1 && {dsq_memory_sev}>=1){
+atten+=1;
+}
+else{
+atten+=0};
+if(memory + atten >= 1){
+cog+=1;
+}
+else{
+cog+=0};
+var pain = 0;
+if({dsq_attention_freq}>=1 && {dsq_attention_freq}>=1){
+pain+=1;
+}
+else{
+pain+=0};
+var immun = 0;
+if({dsq_flu_freq}>=1 && {dsq_flu_freq}>=1){
+immun+=1;
+}
+else{
+immun+=0};
+var symptoms = 0;
+if(pem + sleep + cog + pain + immun>=4){
+symptoms+=1;
+}
+else{
+symptoms+=0};
+var cdc = 0;
+if(fatigue + symptoms==2){
+cdc+=1;
+}
+else{
+cdc+=0};
+cdc;
